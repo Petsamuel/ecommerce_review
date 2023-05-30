e93737c47b33aa54ea6b661cc38f91b950d45562
petsamuel <petsamuel4@gmail.com>
2023-05-30 22:17:41 +0100

    new changes

---
+++ src/components/Popper.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+export const Cart = () => {
+  return (
+    <Fragment>
+      <div className="w-[50%] relative bg-white shadow-2xl">
+        <div>cart</div>
+        <div>Your Cart is Empty</div>
+      </div>
+    </Fragment>
+  );
+};
+
+export const Counter = () => {
+  return <Fragment></Fragment>;
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,35 @@
 import { Fragment } from "react";
 import product from "../assets/image-product-1.jpg";
+import Product_thubnail from "../assets/image-product-1-thumbnail.jpg";
+import Product_thubnail2 from "../assets/image-product-2-thumbnail.jpg";
+import Product_thubnail3 from "../assets/image-product-3-thumbnail.jpg";
+import Product_thubnail4 from "../assets/image-product-4-thumbnail.jpg";
 
 export default function Home() {
+  const Products = [
+    Product_thubnail,
+    Product_thubnail2,
+    Product_thubnail3,
+    Product_thubnail4,
+  ];
+  console.log(Products);
   return (
     <Fragment>
-      <section className="flex justify-center  py-48  gap-24">
+      <section className="flex justify-center  py-[10rem]  gap-24">
         <div className="w-[30%] relative">
           <img src={product} alt="product" className=" rounded-2xl " />
+          <div className="grid grid-cols-4 gap-3  py-6 ">
+            {Products.map((value, key) => {
+              return (
+                <img
+                  src={value}
+                  alt="more-product"
+                  className="rounded-xl  cursor-pointer hover:border-amber-600 hover:bg-opacity-50"
+                  key={key}
+                />
+              );
+            })}
+          </div>
         </div>
         <div className="w-[30%]">
           <div className="mt-6">
