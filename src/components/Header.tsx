6680f43c3ee6799ec57cd0cb38055c8da9d11ef8
petsamuel <petsamuel4@gmail.com>
2023-06-03 22:39:54 +0100

    modified changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import logo from "../assets/logo.svg";
 import cart from "../assets/icon-cart.svg";
 import image from "../assets/image-avatar.png";
 import close from "../assets/icon-close.svg";
 
 export default function Header() {
+  const [active, setIsActive] = useState<boolean>(true);
   const Menu = ["Collections", "Men", "Women", "About", "Contact"];
 
   return (
@@ -12,12 +13,16 @@ export default function Header() {
       <header className="flex justify-around items-center pt-8 fixed w-full z-[50] bg-white">
         <nav className="flex justify-between items-center border-b-2 w-full mx-4 lg:mx-24 relative pb-6">
           <div className="flex relative items-center ">
-            <img src={logo} alt="logo" className="mr-2" />
+            {active ? (
+              <img src={logo} alt="logo" className="mr-2" />
+            ) : (
+              <img src={close} alt="logo" className="mr-2" />
+            )}
             <ul className="bg-white h-screen w-[100%] lg:w-auto lg:h-auto lg:left-0 -left-8 top-0 absolute lg:relative lg:flex gap-5 ml-8  items-center">
               <div></div>
               {Menu.map((value: string | null, key: number | null) => (
                 <li
-                  className="lg:my-auto z-20 lg:py-6 px-0 py-3 font-bold cursor-pointer text-sm lg:font-semi-bold lg:text-gray-400"
+                  className=" bg-white lg:my-auto z-20 lg:py-6 px-0 py-3 font-bold cursor-pointer text-sm lg:font-semi-bold lg:text-gray-400"
                   key={key}
                 >
                   <a href="#" className="">
